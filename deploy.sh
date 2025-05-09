d9b1a0dabe6c4f0c3b29037ff935a5ce5a374fbf
SHM_white <1131287310a@gmail.com>
2025-05-09 16:46:39 +0800

    ...

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 export QMAKE=$1
-
 mkdir AppDir/usr/bin
 mkdir AppDir/usr/lib
-
-if(exist)
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/1-alpha-20250213-1/linuxdeploy-plugin-qt-x86_64.AppImage
 mv linuxdeploy-x86_64.AppImage linuxdeploy
